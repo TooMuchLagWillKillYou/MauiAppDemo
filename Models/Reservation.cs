0712c04c5040904164e1195c17a27f4129db0f2a
Diego Barbaresco <diego.bar@live.it>
2024-04-22 21:41:02 +0200

    added the class library

---
+++ Models/Reservation.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public DateTime Hour { get; set; }
+        public int People { get; set; }
+        public int Table { get; set; }
+        public string? Notes { get; set; }
+    }
+}
