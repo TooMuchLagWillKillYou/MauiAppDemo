3b2d2481bb095a3a4f218f4bb6922e74a956cd36
Diego Barbaresco <diego.bar@live.it>
2024-04-26 09:56:33 +0200

    API call test

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        fetch('https://localhost:7099/api/Reservation/GetReservations')
-            .then(response => response.json())
-            .then(data => console.log("data", data));
-    })
+  useEffect(() => {
+    fetch("https://localhost:7099/api/Reservation/GetReservations")
+      .then((response) => response.json())
+      .then((data) => console.log("data", data));
+  });
 
   return (
     <>
@@ -35,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
