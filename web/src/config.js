b3e25ff739acc7ffdb1777fb1c05f1c53804612e
Diego Barbaresco <diego.bar@live.it>
2024-05-01 16:40:02 +0200

    formatting dateTime for frontend

--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,11 @@
 const config = {
-  baseApiUrl: "https://localhost:4000/",
+  baseApiUrl: "https://localhost:4000",
 };
 
+const dateTimeFormatter = Intl.DateTimeFormat("it-IT", {
+  hour: "numeric",
+  minute: "numeric",
+});
+
 export default config;
+export { dateTimeFormatter };
