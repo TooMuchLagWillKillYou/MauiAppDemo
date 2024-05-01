6aaaf0c43a5b0a0161a64ab2f5a8566ae9a8d037
Diego Barbaresco <diego.bar@live.it>
2024-05-01 19:35:08 +0200

    hooks for CRUD operations

--- web/src/hooks/reservationHooks.js
+++ web/src/hooks/reservationHooks.js
@@ -1,5 +1,5 @@
 import config from "../config";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 // https://tanstack.com/query/latest/docs/framework/react/guides/queries
@@ -14,4 +14,59 @@ const useFetchReservations = () => {
   });
 };
 
+const useFetchReservation = () => {
+  return useQuery({
+    queryKey: ["reservations", id],
+    queryFn: (id) =>
+      axios
+        .get(`${config.baseApiUrl}/reservation/${id}`)
+        .then((response) => response.data)
+        .catch((error) => console.error(error)),
+  });
+};
+
+const useAddReservation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (reservation) =>
+      axios.post(`${config.baseApiUrl}/reservations`, reservation),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["reservations"],
+      });
+    },
+  });
+};
+
+const useUpdateReservation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (reservation) =>
+      axios.put(`${config.baseApiUrl}/reservations`, reservation),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["reservations"],
+      });
+    },
+  });
+};
+
+const useDeleteReservation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (id) => axios.delete(`${config.baseApiUrl}/reservations`, id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["reservations"],
+      });
+    },
+  });
+};
+
 export default useFetchReservations;
+export {
+  useFetchReservation,
+  useAddReservation,
+  useUpdateReservation,
+  useDeleteReservation,
+};
