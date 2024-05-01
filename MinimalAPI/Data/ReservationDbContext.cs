319db0a1f2b7ae1e2795f6dee9c26a1b6eb0ecb6
Diego Barbaresco <diego.bar@live.it>
2024-05-01 11:20:32 +0200

    database scaffolding

---
+++ MinimalAPI/Data/ReservationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalAPI.Data
+{
+    public class ReservationDbContext : DbContext
+    {
+        public DbSet<ReservationEntity> Reservations => Set<ReservationEntity>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ReservationManager;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SeedData.Seed(modelBuilder);
+        }
+    }
+}
