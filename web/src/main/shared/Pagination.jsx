22e8155322e18107c903509005de4f3ee1df144d
Diego Barbaresco <diego.bar@live.it>
2024-07-12 12:02:30 +0200

    refactoring Pagination component

--- web/src/main/shared/Pagination.jsx
+++ web/src/main/shared/Pagination.jsx
@@ -1,34 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import dayjs from "dayjs";
 import { Button } from "@mui/joy";
 import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 import ChevronLeft from "@mui/icons-material/ChevronLeft";
 import ChevronRight from "@mui/icons-material/ChevronRight";
 import "dayjs/locale/it";
-import PaginationModal from "../PaginationModal";
+import PaginationModal from "./PaginationModal";
 
 export default function Pagination() {
   const [currentDate, setCurrentDate] = useState(dayjs());
-  const [isToday, setIsToday] = useState(true);
+  let isToday =
+    currentDate.format("DD/MM/YYYY") == dayjs().format("DD/MM/YYYY");
   const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  useEffect(() => {
-    setIsToday(
-      currentDate.format("DD/MM/YYYY") == dayjs().format("DD/MM/YYYY")
-    );
-  }, [currentDate]);
 
   const getTextToDisplay = () => {
     if (isToday) {
       return "oggi";
     }
-
     return currentDate.locale("it").format("dddd D MMMM");
   };
   const navigate = (daysToAdd) =>
     setCurrentDate(currentDate.add(daysToAdd, "days"));
-  const handleClick = () => setModalIsOpen(true); //setCurrentDate(dayjs());
-  const handleChange = (event) => setCurrentDate(event);
+  const handleClick = () => setModalIsOpen(true);
 
   return (
     <div className="pagination-container">
@@ -49,15 +42,6 @@ export default function Pagination() {
         isOpen={modalIsOpen}
         setIsOpen={setModalIsOpen}
       />
-      {/* <div className="date-picker-container">
-        <StaticDatePicker
-          value={currentDate}
-          minDate={dayjs("2022-06-03")}
-          maxDate={dayjs().add(1, "year")}
-          onChange={handleChange}
-          //shouldDisableDate // prop per gestire i giorni di ferie/chiusura
-        />
-      </div> */}
     </div>
   );
 }
