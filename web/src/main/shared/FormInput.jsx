dc2741298e3b3a8dfa69205b8ffc442ec141aa25
Diego Barbaresco <diego.bar@live.it>
2025-02-25 19:19:54 +0100

    prettier

--- web/src/main/shared/FormInput.jsx
+++ web/src/main/shared/FormInput.jsx
@@ -1,19 +1,26 @@
 import TextField from "@mui/material/TextField";
 
-export default function FormInput({name, label, type, value, onChange, slotProps, sx, errorMessage}) {
-
+export default function FormInput({
+  name,
+  label,
+  type,
+  value,
+  onChange,
+  slotProps,
+  sx,
+  error,
+}) {
   return (
-        <TextField
-            id={name}
-            label={label}
-            type={type}
-            value={value}
-            onChange={onChange}
-            slotProps={slotProps}
-            sx={sx}
-            error={errorMessage?.length}
-            helperText={errorMessage}
-            size="small"
-        />
-  )
+    <TextField
+      id={name}
+      label={label}
+      type={type}
+      value={value}
+      onChange={onChange}
+      slotProps={slotProps}
+      sx={sx}
+      error={error}
+      size="small"
+    />
+  );
 }
