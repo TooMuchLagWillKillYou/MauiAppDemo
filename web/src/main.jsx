f18a849ba621ea26a625fa8a951dfeae23d41525
Diego Barbaresco <diego.bar@live.it>
2025-02-26 21:35:30 +0100

    removed margin from <body>

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "@fontsource/inter";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { itIT } from "@mui/x-date-pickers/locales";
+import "../style.css";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
