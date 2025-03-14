da15370ea167f4f09e2b14557e6414bd748d0a64
Diego Barbaresco <diego.bar@live.it>
2025-03-14 07:12:14 +0100

    prettier

--- web/src/hooks/menuHooks.js
+++ web/src/hooks/menuHooks.js
@@ -1,69 +1,61 @@
-import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import config from "../config.js";
 import axios from "axios";
 
 const useFetchPizzas = () => {
-    return useQuery({
-        queryKey: ["pizzas"],
-        queryFn: () =>
-            axios.get(`${config.baseApiUrl}/pizzas`)
-                .then((response) => response.data)
-                .catch((error) => console.error("useFetchPizzas", error)),
-
-    })
-}
+  return useQuery({
+    queryKey: ["pizzas"],
+    queryFn: () =>
+      axios
+        .get(`${config.baseApiUrl}/pizzas`)
+        .then((response) => response.data)
+        .catch((error) => console.error("useFetchPizzas", error)),
+  });
+};
 
 const useAddPizza = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: (pizza) =>
-            axios.post(`${config.baseApiUrl}/pizzas`, pizza),
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: ["pizzas"]
-            })
-        },
-        onError: (error) => {
-            console.log("useAddPizza error", error.response.data.errors);
-        },
-    })
-}
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (pizza) => axios.post(`${config.baseApiUrl}/pizzas`, pizza),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["pizzas"],
+      });
+    },
+    onError: (error) => {
+      console.log("useAddPizza error", error.response.data.errors);
+    },
+  });
+};
 
 const useUpdatePizza = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: (pizza) =>
-            axios.put(`${config.baseApiUrl}/pizzas`, pizza),
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: ["pizzas"],
-            });
-        },
-        onError: (error) => {
-            console.log("useUpdatePizza error", error.response.data.errors);
-        },
-    })
-}
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (pizza) => axios.put(`${config.baseApiUrl}/pizzas`, pizza),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["pizzas"],
+      });
+    },
+    onError: (error) => {
+      console.log("useUpdatePizza error", error.response.data.errors);
+    },
+  });
+};
 
 const useDeletePizza = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: (id) =>
-            axios.delete(`${config.baseApiUrl}/pizzas/${id}`),
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: ["pizzas"],
-            });
-        },
-        onError: (error) => {
-            console.log("useDeletePizza error", error.response.data.errors);
-        },
-    })
-}
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (id) => axios.delete(`${config.baseApiUrl}/pizzas/${id}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["pizzas"],
+      });
+    },
+    onError: (error) => {
+      console.log("useDeletePizza error", error.response.data.errors);
+    },
+  });
+};
 
-export {
-    useFetchPizzas,
-    useAddPizza,
-    useUpdatePizza,
-    useDeletePizza
-}
+export { useFetchPizzas, useAddPizza, useUpdatePizza, useDeletePizza };
--- web/src/pages/menu/Menu.jsx
+++ web/src/pages/menu/Menu.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import { Box, Typography } from "@mui/material";
 
 export default function Menu() {
   return (
-      <React.Fragment>
-          <Box sx={{ mb: 1 }}>
-              <Typography level="h2" component="h1">
-                  Prenotazioni
-              </Typography>
-          </Box>
-      </React.Fragment>
+    <React.Fragment>
+      <Box sx={{ mb: 1 }}>
+        <Typography level="h2" component="h1">
+          Menù
+        </Typography>
+      </Box>
+    </React.Fragment>
   );
 }
