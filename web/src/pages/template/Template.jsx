5bd0457bfe4ff13bfe4fb021a9a54ab7473345e8
Diego Barbaresco <diego.bar@live.it>
2025-04-17 23:40:35 +0200

    template mockup

--- web/src/pages/template/Template.jsx
+++ web/src/pages/template/Template.jsx
@@ -4,31 +4,27 @@ import { Box } from "@mui/material";
 import { Page, Title, Container, Group } from "../../utils/menuComponents.jsx";
 
 export default function Template() {
-  const pizzas = [
-    {
-      title: "Marinara",
-      ingredients:
-        "pomodoro pelato italiano macerato a mano, origano calabrese, aglio tritato fine, olio extravergine di oliva 100% italiano, basilico fresco",
-      price: 6.5,
-    },
-    {
-      title: "Bufala",
-      ingredients:
-        "pomodoro pelato San Marzano Dop, mozzarella di bufala campana DOP, parmigiano reggiano, olio extravergine 100% italiano, basilico fresco",
-      price: 10.5,
-    },
-  ];
+  const dummy = {
+    title: "Title",
+    ingredients:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    price: 10.5,
+  };
 
   return (
     <React.Fragment>
       <PageTitle text="Template" />
       <Box>
-        <Page className="a5-sheet">
+        <Page className="a5-sheet" id="first-page">
           <Title>Le Specialita'</Title>
           <Container>
-            {pizzas.map((pizza, i) => (
-              <Group {...pizza} key={`menu-group-${i}`} />
-            ))}
+            <Group {...dummy} />
+          </Container>
+        </Page>
+        <Page className="a5-sheet">
+          <Title>Le Classiche</Title>
+          <Container>
+            <Group {...dummy} />
           </Container>
         </Page>
       </Box>
