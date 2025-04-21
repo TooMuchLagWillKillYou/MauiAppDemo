ededcb830311cb84541c48c5f3723b39e27341ed
Diego Barbaresco <diego.bar@live.it>
2025-04-21 13:04:11 +0200

    implementation for the abstract class BaseMenuGenerator

--- MinimalAPI/Services/MenuGenerator/BaseMenuGenerator.cs
+++ MinimalAPI/Services/MenuGenerator/BaseMenuGenerator.cs
@@ -1,3 +1,6 @@
+using iText.Html2pdf;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
 using MinimalAPI.Common;
 using MinimalAPI.Data.Repositories;
 
@@ -6,20 +9,44 @@ namespace MinimalAPI.Services.MenuGenerator;
 public abstract class BaseMenuGenerator(MenuLanguage language, IPizzaRepository repository)
 {
     private readonly MenuLanguage _language = language;
-
+    public string Html { get; set; }
     public byte[] Generate()
     {
         GetData();
-        CreateCover();
-        WriteTitle();
+        WriteCoverHtml();
+        WriteTitlesHtml();
+        // WritePizzasHtml();
+        // WriteAperitifsHtml();
+        // WriteBeverageHtml();
+        // WriteBeersHtml();
+        // WriteWinesHtml();
         return GeneratePdf();
     }
 
     protected virtual void GetData()
     {
+    }
+    protected virtual void WriteCoverHtml()
+    {
+
+    }
+    protected virtual void WriteTitlesHtml()
+    {
         
     }
-    protected abstract void CreateCover();
-    protected abstract void WriteTitle();
-    protected abstract byte[] GeneratePdf();
+    protected virtual byte[] GeneratePdf()
+    {
+        var htmlString = """
+                         <div>Hello world</div>
+                         """;
+        using var stream = new MemoryStream();
+        var writer = new PdfWriter(stream);
+        var pdf = new PdfDocument(writer);
+        pdf.SetDefaultPageSize(PageSize.A5);
+        
+        ConverterProperties props = new ConverterProperties();
+        HtmlConverter.ConvertToPdf(htmlString, pdf, props);
+        
+        return stream.ToArray();
+    }
 }
--- MinimalAPI/Services/MenuGenerator/CompleteMenuGenerator.cs
+++ MinimalAPI/Services/MenuGenerator/CompleteMenuGenerator.cs
@@ -5,12 +5,4 @@ namespace MinimalAPI.Services.MenuGenerator;
 
 public class CompleteMenuGenerator(MenuLanguage language, IPizzaRepository repository) : BaseMenuGenerator(language, repository)
 {
-    protected override void GetData()
-        => throw new NotImplementedException();
-    protected override void CreateCover()
-        => throw new NotImplementedException();
-    protected override void WriteTitle()
-        => throw new NotImplementedException();
-    protected override byte[] GeneratePdf()
-        => throw new NotImplementedException();
 }
