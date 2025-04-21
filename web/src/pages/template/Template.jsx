a31a08dd18f73f11bd04b9895beabcd51802482a
Diego Barbaresco <diego.bar@live.it>
2025-04-21 13:17:24 +0200

    re-wrote menu components in plain css

--- web/src/pages/template/Template.jsx
+++ web/src/pages/template/Template.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
-import PageTitle from "../../shared/PageTitle.jsx";
+import React, { useEffect, useRef, useState } from "react";
 import { Box } from "@mui/material";
-import { Page, Title, Container, Group } from "../../utils/menuComponents.jsx";
+import {
+  Page,
+  PageTitle,
+  Container,
+  PizzaGroup,
+} from "../../utils/menuComponents.jsx";
 import { useFetchPizzas } from "../../hooks/menuHooks.js";
 import ApiStatus from "../../utils/ApiStatus.jsx";
 
 export default function Template() {
   const { data, status, isSuccess } = useFetchPizzas();
+
   if (!isSuccess) {
     return <ApiStatus status={status} />;
   }
@@ -14,11 +19,11 @@ export default function Template() {
     <React.Fragment>
       <PageTitle text="Template" />
       <Box>
-        <Page className="a5-sheet" id="first-page">
-          <Title>Le Specialita'</Title>
+        <Page>
+          <PageTitle>Le Specialita'</PageTitle>
           <Container>
             {data?.map((item, index) => (
-              <Group {...item} key={index} />
+              <PizzaGroup {...item} key={index} />
             ))}
           </Container>
         </Page>
--- web/src/utils/menuComponents.jsx
+++ web/src/utils/menuComponents.jsx
@@ -1,55 +1,13 @@
-import { styled } from "@mui/material/styles";
-import { Box, Typography } from "@mui/material";
-
-const Page = styled(Box)(({ theme }) => ({
-  width: "148mm",
-  height: "210mm",
-  background: "white",
-  margin: "auto",
-  boxShadow: "0 0 10px rgba(0,0,0,0.1)",
-  padding: "10mm",
-  boxSizing: "border-box",
-}));
-
-const Title = styled(Typography)(({ theme }) => ({
-  fontFamily: "BrolimoRegular",
-  fontSize: "24pt",
-  textAlign: "center",
-  marginBottom: "15pt",
-}));
-
-const Container = styled(Box)(({ theme }) => ({
-  display: "flex",
-  height: "100%",
-  flexDirection: "column",
-  gap: "15pt",
-}));
-
-const Group = (props) => (
-  <Box>
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "end",
-      }}
-    >
-      <Typography
-        sx={{ fontFamily: "Montserrat", fontSize: "17pt", fontWeight: 700 }}
-      >
-        {props.name}
-      </Typography>
-      <Typography sx={{ fontFamily: "BrolimoRegular", fontSize: "14pt" }}>
-        {props.price}
-      </Typography>
-    </Box>
-    <Typography
-      component="p"
-      sx={{ fontFamily: "Montserrat", fontSize: "12pt" }}
-    >
-      {props.ingredients}
-    </Typography>
-  </Box>
+const Page = (props) => <div {...props} className="page" />;
+const PageTitle = (props) => <p {...props} className="pageTitle" />;
+const Container = (props) => <div {...props} className="menuItemsContainer" />;
+const PizzaGroup = (props) => (
+  <div className="pizzaGroup">
+    <div className="pizzaHeader">
+      <p className="pizzaName">{props.name}</p>
+      <p className="pizzaPrice">{props.price}</p>
+    </div>
+    <p className="pizzaIngredients">{props.ingredients}</p>
+  </div>
 );
-
-export { Page, Title, Container, Group };
+export { Page, PageTitle, Container, PizzaGroup };
