1da4c54324a9083695d1459aeaf10b2a33ad0733
tooMuchLagWillKilYou <diego.bar@live.it>
2025-11-12 14:57:55 +0100

    minor fix

--- spa/src/components/pagination.tsx
+++ spa/src/components/pagination.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { ArrowLeftIcon, ArrowRightIcon } from 'lucide-react';
-import { sub, add, format } from 'date-fns';
+import { sub, add, format, isToday } from 'date-fns';
 import { TypographyH1 } from './ui/typography-h1';
 import { cn } from '@/lib/utils';
 
@@ -10,24 +10,22 @@ interface PaginationProps {
   classNames?: string;
 }
 function Pagination({ date, setDate, classNames }: PaginationProps) {
-  console.log(date);
-
   return (
     <div className={cn('flex justify-between', classNames)}>
       <Button
         variant="outline"
         size="icon"
         aria-label="Pagination left"
-        onClick={() => setDate(sub(new Date(date), { days: 1 }))}
+        onClick={() => setDate(sub(date, { days: 1 }))}
       >
         <ArrowLeftIcon />
       </Button>
-      <TypographyH1 text={format(date, 'eeee d')} />
+      <TypographyH1 text={isToday(date) ? 'Today' : format(date, 'eeee d')} />
       <Button
         variant="outline"
         size="icon"
         aria-label="Pagination right"
-        onClick={() => setDate(add(new Date(date), { days: 1 }))}
+        onClick={() => setDate(add(date, { days: 1 }))}
       >
         <ArrowRightIcon />
       </Button>
